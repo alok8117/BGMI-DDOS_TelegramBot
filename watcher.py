590964fe739f13d2355efdc611ab29ad3e354c7e
alok8117 <alokdebbarma180@gmail.com>
2024-12-28 18:29:53 +0530

    Update watcher.py

--- watcher.py
+++ watcher.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = '7262217536:AAHX6ROfNRk1ZH9ZcwokcwvyR4jFtk3fKP8'
-ADMIN_ID = '1065510487'
+API_TOKEN = '7592440711:AAEwHh_QHm0bA2ogkdAUQIBaab6MWvk2ie0'
+ADMIN_ID = '7471974865'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
